fdf391a587ffcf71b9541e9e663c79b5cf0aa1a1
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-01-14 14:51:14 +0530

    fix CounterSlice import path

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterRudecer from './app/counterRudecer' 
+import { configureStore } from "@reduxjs/toolkit"
+import counterReducer from "./CounterSlice"
 
 export const store = configureStore({
   reducer: {
-   counter: counterRudecer,
-  },
+    counter: counterReducer
+  }
 })
